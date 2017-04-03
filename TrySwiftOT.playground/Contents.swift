3da8ef7110809d369f0c1b46512bf8ef5e68ca99
Jose Marchena <jose.marchena@ocado.com>
2017-04-03 15:39:06 +0200

    Types, constants and variables

---
+++ TrySwiftOT.playground/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+/*:
+##### Types, constants and variables
+
+*/
+/*:
+* Constants are deifned by `let` and variables by `var`
+* Swift has type inference, you can check the type doing alt+click over the varaible name
+* We can define the type of a variable/constant explicitly
+ */
+
+let numberOfFloors = 2
+
+var favoriteDeliveryService = "Glovo"
+
+let bestProgrammingLanguage: String = "Swift"
+
+
+/*:
+ Use print to print to the console
+ String interpolation `\()` to print values
+ */
+
+print("Our favourite food delivery service is \(favoriteDeliveryService)")
